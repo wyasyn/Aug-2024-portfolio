4c308e1629c95f52c1064a4e3ffb73f69b8da6b7
Yasin Walum <ywalum@gmail.com>
2024-08-30 01:36:33 +0300

    image loading

---
+++ components/ImageLoading.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function ImageLoading() {
+  return <Skeleton className='aspect-[4/3] w-[300px] rounded-xl' />
+}
--- components/StarterPlaceholder.tsx
+++ components/StarterPlaceholder.tsx
@@ -3,6 +3,8 @@ import { Button } from './ui/button'
 import { SendHorizontal } from 'lucide-react'
 import CustomeImage from './CustomeImage'
 import BackgroundBlur from './BackgroundBlur'
+import { Suspense } from 'react'
+import ImageLoading from './ImageLoading'
 
 export default function StarterPlaceholder() {
   return (
@@ -15,17 +17,19 @@ export default function StarterPlaceholder() {
         color='primary'
       />
       <div className='flex flex-col items-center gap-4'>
-        <CustomeImage
-          width='w-[300px]'
-          title='placeholder'
-          src='https://images.unsplash.com/photo-1629904869392-ae2a682d4d01?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        />
-        <h1 className='font-lato text-balance text-center text-4xl font-bold md:text-5xl'>
+        <Suspense fallback={<ImageLoading />}>
+          <CustomeImage
+            width='w-[300px]'
+            title='placeholder'
+            src='https://images.unsplash.com/photo-1629904869392-ae2a682d4d01?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+          />
+        </Suspense>
+        <h1 className='text-balance text-center font-lato text-4xl font-bold md:text-5xl'>
           Welcome to your new Next.js app!
         </h1>
         <p className='text-center'>
           Get started by editing{' '}
-          <code className='text-accent font-mono'>app/page.tsx</code> or{' '}
+          <code className='font-mono text-accent'>app/page.tsx</code> or{' '}
           <code>components/StarterPlaceholder.tsx</code>.
         </p>
         <Button size='sm'>
